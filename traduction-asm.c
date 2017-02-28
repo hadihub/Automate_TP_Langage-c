06a8f7e42b6e6eee4a936741515c6a424c289de0
hadihub <mh.elhadi@outlook.com>
2017-02-28 11:57:08 +0100

    Ajout des fichiers assembleur et gitignore

---
+++ traduction-asm.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "traduction-asm.h"
+#include "table-symboles.h"
+
+int indice_tempo_max = 0 ;
+
+/*************** Fonctions privées ***************/
+
+//return le numéro de la variable temporaire
+int reserver_temporaire()
+{
+	int i ;
+	int trouve = 0 ;	
+}
+
+/*************** Fonctions publiques ***************/
+
+void expr_nb (int nb)
+{
+	printf("AFC \n") ;
+}
---
+++ traduction-asm.h
@@ -0,0 +1,6 @@
+#ifndef TRADUC_ASM_H
+#define TRADUC_ASM_H
+
+void expr_nb () ;
+
+#endif // TRADUC_ASM_H
